640ddd18566eb139bb0aa517ce44f38af5145535
Jaishree <jaishreeofficial29@gmail.com>
2025-01-26 14:10:42 +0530

    More about slices

--- 08slices/main.go
+++ 08slices/main.go
@@ -27,4 +27,12 @@ func main() {
 	highScores = append(highScores, 123, 23, 322) //appends the values by adding more memory
 	sort.Ints(highScores) //Sorts the integer in ascending order
 	fmt.Println(highScores)
+
+	// How to remove a value from slices based on index
+	var courses = []string{"reactjs", "python", "javascript", "swift"}
+	fmt.Println(courses)
+	var index = 2
+	courses = append(courses[:index], courses[index+1:]...)
+	// the three dots at the ends are used when there are more arguments used than expected
+	fmt.Println(courses)
 }
