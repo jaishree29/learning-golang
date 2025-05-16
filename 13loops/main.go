c4ffb02185be007e23d57e7d2118deb687375998
Jaishree <jaishreeofficial29@gmail.com>
2025-05-16 20:34:15 +0530

    Learned more about loops

--- 13loops/main.go
+++ 13loops/main.go
@@ -1,25 +1,54 @@
 package main
 
+import "fmt"
+
 func main() {
-	// for loop
-	for i := 0; i < 10; i++ {
-		println(i)
-	}
+	// // for loop
+	// for i := 0; i < 10; i++ {
+	// 	fmt.Println(i)
+	// }
 
-	// while loop
-	i := 0
-	for i < 10 {
-		println(i)
-		i++
-	}
+	// // for loop with range
+	// days := []string{"Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"}
+	// for i := range days {
+	// 	fmt.Println(i, days[i])
+	// }
 
-	// do while loop
-	i = 0
-	for {
-		if i >= 10 {
-			break
+	// for index, day := range days {
+	// 	fmt.Printf("Index is %v and day is %v\n", index, day)
+	// }
+
+	// for loop with condition
+	rougeValue := 1;
+	for rougeValue < 10 {
+		if rougeValue == 2 {
+			goto lco
 		}
-		println(i)
-		i++
+		if rougeValue == 5 {
+			rougeValue++
+			continue
+		}
+		fmt.Println(rougeValue)
+		rougeValue++
 	}
+
+	lco:
+	fmt.Print("This is a label")
+
+	// // while loop
+	// i := 0
+	// for i < 10 {
+	// 	println(i)
+	// 	i++
+	// }
+
+	// // do while loop
+	// i = 0
+	// for {
+	// 	if i >= 10 {
+	// 		break
+	// 	}
+	// 	println(i)
+	// 	i++
+	// }
 }
